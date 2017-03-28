3a20e756e9100fb107bf240f6c8156960a684547
Polona Caserman <polona.caserman@gmail.com>
2017-03-28 14:02:58 +0200

    Initial commit

---
+++ BodyModel/Sources/Main.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+
+#include <Kore/Graphics/Graphics.h>
+#include <Kore/Input/Keyboard.h>
+#include <Kore/System.h>
+#include <Kore/Log.h>
+
+using namespace Kore;
+
+namespace {
+    
+    bool left;
+    bool right;
+    bool down;
+    bool up;
+    
+	void update() {
+		Graphics::begin();
+        
+        Graphics::end();
+		Graphics::swapBuffers();
+	}
+
+	void keyDown(KeyCode code, wchar_t character) {
+		switch (code) {
+            case Kore::Key_Left:
+				left = true;
+				break;
+			case Kore::Key_Right:
+				right = true;
+				break;
+			case Kore::Key_Down:
+				down = true;
+				break;
+			case Kore::Key_Up:
+				up = true;
+				break;
+            default:
+				break;
+        }
+	}
+
+	void keyUp(KeyCode code, wchar_t character) {
+		switch (code) {
+            case Kore::Key_Left:
+				left = false;
+				break;
+			case Kore::Key_Right:
+				right = false;
+				break;
+			case Kore::Key_Down:
+				down = false;
+				break;
+			case Kore::Key_Up:
+				up = false;
+				break;
+            default:
+                break;
+        }
+	}
+}
+
+int kore(int argc, char** argv) {
+    int w = 1024;
+    int h = 512;
+
+	System::init("BodyTracking", w, h);
+    
+	System::setCallback(update);
+
+	Keyboard::the()->KeyDown = keyDown;
+	Keyboard::the()->KeyUp = keyUp;
+	
+    System::start();
+    
+    System::stop();
+
+	return 0;
+}
---
+++ BodyModel/korefile.js
@@ -0,0 +1,9 @@
+var project = new Project('BodyTracking', __dirname);
+
+project.addFile('Sources/**');
+project.setDebugDir('Deployment');
+
+Project.createProject('Kore', __dirname).then((subproject) => {
+	project.addSubProject(subproject);
+	resolve(project);
+});
