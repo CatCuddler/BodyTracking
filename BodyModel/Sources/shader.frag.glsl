d4e0fc6f90bab3d3c82ed352a2b45ec77a8d6ff9
Polona Caserman <polona.caserman@gmail.com>
2017-09-22 17:52:43 +0200

    Update shader

--- BodyModel/Sources/shader.frag.glsl
+++ BodyModel/Sources/shader.frag.glsl
@@ -14,5 +14,4 @@ out vec4 FragColor;
 
 void kore() {
 	FragColor = texture(tex, texCoord);
-	//gl_FragColor = vec4(((texture2D(tex, texCoord) * (0.8 + max(dot(vec3(0, 1, 0), normal), 0.0) * 0.2)).xyz), 1);
 }
