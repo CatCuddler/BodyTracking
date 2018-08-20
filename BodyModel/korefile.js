56eed281004c958a7c93c63c593c20232fc7782e
Polona Caserman <polona.caserman@gmail.com>
2018-08-20 14:39:41 +0200

    Update korefile

--- BodyModel/korefile.js
+++ BodyModel/korefile.js
@@ -3,7 +3,7 @@ var project = new Project('BodyTracking', __dirname);
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
 
-project.addIncludeDir('../HMM_Trainer/HMM_Trainer');
+project.addIncludeDir('../HMM_Trainer/Sources');
 
 Project.createProject('Kore', __dirname).then((subproject) => {
 	project.addSubProject(subproject);
