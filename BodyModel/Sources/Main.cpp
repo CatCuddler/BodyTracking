22ce750b2ba745d6b09af3dd7069e127cbbf1bd5
Polona Caserman <polona.caserman@gmail.com>
2019-01-19 22:23:59 +0100

    Load the same avatar for a desktop or VR application

--- BodyModel/Sources/Main.cpp
+++ BodyModel/Sources/Main.cpp
@@ -767,12 +767,7 @@ namespace {
 	
 	void init() {
 		loadAvatarShader();
-		
-#ifdef KORE_STEAMVR
 		avatar = new Avatar("avatar/avatar.ogex", "avatar/", structure);
-#else
-		avatar = new Avatar("avatar/avatar.ogex", "avatar/", structure);
-#endif
 		
 		initTransAndRot();
 		
