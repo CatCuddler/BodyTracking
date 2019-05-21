4d6526b938a1b0d2b564c670130b6f13ddef3b66
Polona Caserman <polona.caserman@gmail.com>
2019-05-21 15:14:56 +0200

    Delete unused tags

--- BodyModel/Sources/EndEffector.cpp
+++ BodyModel/Sources/EndEffector.cpp
@@ -66,17 +66,13 @@ void EndEffector::setIKMode(IKMode mode) {
 
 const char* EndEffector::getNameForIndex(const int ID) const {
 	if (ID == headBoneIndex)				return headTag;
-	else if (ID == spineBoneIndex)			return spineTag;
 	else if (ID == hipBoneIndex)			return hipTag;
 	else if (ID == leftHandBoneIndex)		return lHandTag;
 	else if (ID == rightHandBoneIndex)		return rHandTag;
 	else if (ID == leftForeArmBoneIndex)	return lForeArm;
 	else if (ID == rightForeArmBoneIndex)	return rForeArm;
-	else if (ID == rightArmBoneIndex)		return rArm;
-	else if (ID == leftFootBoneIndex)		return lFootTag;
-	else if (ID == rightFootBoneIndex)		return rFootTag;
-	else if (ID == leftLegBoneIndex)		return lLeg;
-	else if (ID == rightLegBoneIndex)		return rLeg;
+	else if (ID == leftLegBoneIndex)		return lLegTag;
+	else if (ID == rightLegBoneIndex)		return rLegTag;
 	else return nullptr;
 }
 
@@ -85,7 +81,7 @@ int EndEffector::getIndexForName(const char* name) const {
 	else if(std::strcmp(name, hipTag) == 0)		return hipBoneIndex;
 	else if(std::strcmp(name, lHandTag) == 0)	return leftHandBoneIndex;
 	else if(std::strcmp(name, rHandTag) == 0)	return rightHandBoneIndex;
-	else if(std::strcmp(name, lFootTag) == 0)	return leftFootBoneIndex;
-	else if(std::strcmp(name, rFootTag) == 0)	return rightFootBoneIndex;
+	else if(std::strcmp(name, lLegTag) == 0)	return leftFootBoneIndex;
+	else if(std::strcmp(name, rLegTag) == 0)	return rightFootBoneIndex;
 	else return -1;
 }
