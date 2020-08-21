94f18dc78ef03c4c459bdf0df0b79ec0f9dd06bb
Polona Caserman <polona.caserman@gmail.com>
2020-08-21 17:04:03 +0200

    Update korefile

--- BodyModel/korefile.js
+++ BodyModel/korefile.js
@@ -3,10 +3,4 @@ var project = new Project('BodyTracking', __dirname);
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
 
-project.addIncludeDir('../HMM_Trainer/Sources');
-project.addFile('../HMM_Trainer/Sources/kMeans.cpp');
-project.addFile('../HMM_Trainer/Sources/kMeans.h');
-project.addFile('../HMM_Trainer/Sources/Markov.cpp');
-project.addFile('../HMM_Trainer/Sources/Markov.h');
-
 resolve(project);
