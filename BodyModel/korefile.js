6f45156c10d8abff6403a7ce7c14740db97901ea
Polona Caserman <polona.caserman@gmail.com>
2020-08-23 18:39:49 +0200

    Update korefile

--- BodyModel/korefile.js
+++ BodyModel/korefile.js
@@ -3,14 +3,14 @@ var project = new Project('BodyTracking', __dirname);
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
 
-if (platform === Platform.Windows) {
-	// Needed for Hidden Markov Models
-	project.addIncludeDir('../HMM_Trainer/Sources');
-	project.addFile('../HMM_Trainer/Sources/kMeans.cpp');
-	project.addFile('../HMM_Trainer/Sources/kMeans.h');
-	project.addFile('../HMM_Trainer/Sources/Markov.cpp');
-	project.addFile('../HMM_Trainer/Sources/Markov.h');
+// Needed for Hidden Markov Models
+project.addIncludeDir('../HMM_Trainer/Sources');
+project.addFile('../HMM_Trainer/Sources/kMeans.cpp');
+project.addFile('../HMM_Trainer/Sources/kMeans.h');
+project.addFile('../HMM_Trainer/Sources/Markov.cpp');
+project.addFile('../HMM_Trainer/Sources/Markov.h');
 
+if (platform === Platform.Windows) {
 	// Needed for motion recognition with Weka
 	project.addIncludeDir('../zulu14.29.23-ca-jdk14.0.2-win_x64/include');
 	project.addIncludeDir('../zulu14.29.23-ca-jdk14.0.2-win_x64/include/win32');
