3ef3e3ca28957758744cb7880376575c50dfba17
Polona Caserman <polona.caserman@gmail.com>
2020-11-20 07:46:30 +0100

    Add new eval data

--- BodyModel/Sources/InverseKinematics.cpp
+++ BodyModel/Sources/InverseKinematics.cpp
@@ -49,17 +49,14 @@ void InverseKinematics::inverseKinematics(BoneNode* targetBone, IKMode ikMode, K
 		prevDeltaTheta = deltaTheta;
 		
 		// todo: better!
-		if ((!simpleIK && (targetBone->nodeIndex == leftForeArmBoneIndex || targetBone->nodeIndex == rightForeArmBoneIndex)) ||
-			 (simpleIK && (targetBone->nodeIndex == leftHandBoneIndex || targetBone->nodeIndex == rightHandBoneIndex))) {
-			if (simpleIK) {
-				deltaTheta = jacobianSimpleIKHand->calcDeltaTheta(targetBone, desPosition, desRotation, ikMode);
-				errorPos = jacobianSimpleIKHand->getPositionError();
-				errorRot = jacobianSimpleIKHand->getRotationError();
-			} else {
-				deltaTheta = jacobianHand->calcDeltaTheta(targetBone, desPosition, desRotation, ikMode);
-				errorPos = jacobianHand->getPositionError();
-				errorRot = jacobianHand->getRotationError();
-			}
+		if (simpleIK && (targetBone->nodeIndex == leftHandBoneIndex || targetBone->nodeIndex == rightHandBoneIndex)) {
+			deltaTheta = jacobianSimpleIKHand->calcDeltaTheta(targetBone, desPosition, desRotation, ikMode);
+			errorPos = jacobianSimpleIKHand->getPositionError();
+			errorRot = jacobianSimpleIKHand->getRotationError();
+		} else if (!simpleIK && (targetBone->nodeIndex == leftForeArmBoneIndex || targetBone->nodeIndex == rightForeArmBoneIndex)) {
+			deltaTheta = jacobianHand->calcDeltaTheta(targetBone, desPosition, desRotation, ikMode);
+			errorPos = jacobianHand->getPositionError();
+			errorRot = jacobianHand->getRotationError();
 		} else if (targetBone->nodeIndex == leftFootBoneIndex|| targetBone->nodeIndex == rightFootBoneIndex) {
 			deltaTheta = jacobianFoot->calcDeltaTheta(targetBone, desPosition, desRotation, ikMode);
 			errorPos = jacobianFoot->getPositionError();
